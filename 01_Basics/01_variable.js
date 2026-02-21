f9abbb5d72348da3a1a15127246866cd023f10d7
Vaishnavi Shinde <shindevaishnavi992@gmail.com>
2026-02-21 06:38:38 +0000

    01_variable.js

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 120589
+let accountEmail = "vaishnavi@gmail.com"
+var accountPassword = "123056"
+accountCity = "Jaipur"
+let userAccount;
+
+// accountId = 100 //not allowed
+// console.log(accountId);
+
+accountEmail = "vaishnavi123@gmail.com";
+console.log(accountEmail);
+
+accountPassword = "2005"
+console.log(accountPassword)
+
+accountCity = "Indore"
+console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,userAccount]);
+
+
+
---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,26 @@
+let name = "Vaishnavi"
+let age = 19
+let answer = true
+let city;
+let picture = null
+
+// number => 2 to power 53 
+// BigInt
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => only declare variable but not value
+
+//object
+
+console.log(typeof ""); //typeof used for find the type of variable
+console.log(typeof age);
+console.log(typeof answer)
+console.log(typeof city);
+console.log(typeof picture)
+
+/*
+    undefined is typeof undefined but null is typeof object
+*/
+
+
