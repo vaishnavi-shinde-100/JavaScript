abbb06227790ef16f3e23e7489551d8e1fcb486d
Vaishnavi Shinde <shindevaishnavi992@gmail.com>
2026-02-21 07:24:19 +0000

    03_conversionOperation.js

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,19 +1,19 @@
-// let score = 33
+let score = 33;
 
 // console.log(typeof score);  //number
 // console.log(typeof (score)); //number
 
-// score = "33"
+score = "33";
 // console.log(typeof score); //string
 
-// score = "33abc10"
+score = "33abc10";
 // console.log(typeof score); //string
 
-// let valueInNumber = Number (score) 
+let valueInNumber = Number(score);
 // console.log(typeof valueInNumber); //number
 // console.log(valueInNumber); //NaN- Not A Number
 
-// let isLogged = "vaishnavi"
+let isLogged = "vaishnavi";
 // let NumberOfIsLogged = Boolean (isLogged)
 // console.log(NumberOfIsLogged);
 
@@ -22,12 +22,48 @@
 //"" => false
 //"vaishnavi" => true
 
-let takeNumber = 100
-let takeNumberIntoString = String(takeNumber)
-console.log(takeNumberIntoString)
-console.log(typeof takeNumberIntoString)
+let takeNumber = 100;
+let takeNumberIntoString = String(takeNumber);
+// console.log(takeNumberIntoString)
+// console.log(typeof takeNumberIntoString)
 
+/*--------------------------OPERATION-----------------------*/
 
+let str1 = "Hello";
+let str2 = " Vaishnavi:)";
+let str3 = str1 + str2;
+// console.log(str3);
 
+// console.log(2+3)
+// console.log(10-4)
+// console.log(10*5);
+// console.log(10/5);
+// console.log(2%3);
+
+// console.log("1"+2); //12
+// console.log(1+"2")  //12
+// console.log("1"+2+2) //122
+// console.log(2+2+"1") //41
+// console.log(true) //true
+// console.log(+true) //1
+// console.log(true+) //error
+
+let counter = 100;
+// counter++
+// console.log(counter); //101
+// ++counter
+// console.log(counter); //101
+
+// console.log(counter++) //100
+// console.log(counter);//101
+
+// console.log(++counter); //101
+// console.log(counter); //101
+
+// console.log(counter--) //100
+// console.log(counter);//99
+
+console.log(--counter) //99
+console.log(counter);//99
 
 
