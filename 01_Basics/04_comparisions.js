699d8f316ae27cc932bcd0e8df2f737bfd158ffa
Vaishnavi Shinde <shindevaishnavi992@gmail.com>
2026-02-21 07:37:21 +0000

    04_comparisions.js

---
+++ 01_Basics/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log(2>1)
+// console.log(4<5)
+// console.log(4>=8)
+// console.log(5<=8);
+// console.log(4==4)
+//console.log(4!=4)
+
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null > 0) //false
+// console.log(null < 0) //false
+// console.log(null == 0) //false
+// console.log(null >= 0) //true
+// console.log(null <= 0) //true
+
+// console.log(undefined > 0) //false
+// console.log(undefined < 0) //false
+// console.log(undefined == 0) //false
+// console.log(undefined >= 0) //false
+// console.log(undefined <= 0) //false
+
+//=== 
+/* treeple equal to compare the datatype of variables */
+console.log("2"=== 2)//false
+console.log("2"== 2) //true //double equal to is convert data type
