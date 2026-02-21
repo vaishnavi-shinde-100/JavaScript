dc745d6f5bbd6cf7b1d4c09be70485cfd59df3e3
Vaishnavi Shinde <shindevaishnavi992@gmail.com>
2026-02-21 06:39:43 +0000

    02_dataType.js 03_conversionOperation.js

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = 33
+
+// console.log(typeof score);  //number
+// console.log(typeof (score)); //number
+
+// score = "33"
+// console.log(typeof score); //string
+
+// score = "33abc10"
+// console.log(typeof score); //string
+
+// let valueInNumber = Number (score) 
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN- Not A Number
+
+// let isLogged = "vaishnavi"
+// let NumberOfIsLogged = Boolean (isLogged)
+// console.log(NumberOfIsLogged);
+
+//1  => true
+//0  => false
+//"" => false
+//"vaishnavi" => true
+
+let takeNumber = 100
+let takeNumberIntoString = String(takeNumber)
+console.log(takeNumberIntoString)
+console.log(typeof takeNumberIntoString)
+
+
+
+
+
